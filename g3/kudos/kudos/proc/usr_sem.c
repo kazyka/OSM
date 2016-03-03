6884fb277297e658a4898e1bd205ac17cc4d9b5e
Archimedes of Syracuse <archimedes@syracuse.(none)>
2016-03-03 16:50:52 +0100

    Debugging Destroy. Value not saved

--- g3/kudos/kudos/proc/usr_sem.c
+++ g3/kudos/kudos/proc/usr_sem.c
@@ -11,15 +11,16 @@ static usr_sem_t usr_sem_table[MAX_SEMAPHORES];
 
 usr_sem_t* usr_sem_open(const char* name, int value) {
 
-  //spinlock_acquire(&sem_table_slock);
+  spinlock_acquire(&sem_table_slock);
 
   if (value >= 0) {
-
     for (int i = 0; i < MAX_SEMAPHORES; i++) {
-      if (stringcmp(usr_sem_table[i].name, name)) {
-
-        //spinlock_release(&sem_table_slock);
-        return NULL;
+      if (usr_sem_table[i].name != NULL) {
+        if (!stringcmp(usr_sem_table[i].name, name)) {
+          kprintf(usr_sem_table[i].name);
+          spinlock_release(&sem_table_slock);
+          return NULL;
+        }
       }
     }
 
@@ -27,71 +28,82 @@ usr_sem_t* usr_sem_open(const char* name, int value) {
       if (usr_sem_table[j].name == NULL) {
         usr_sem_table[j].name = name;
         usr_sem_table[j].value = value;
-
-        //spinlock_release(&sem_table_slock);
+        kprintf("Value of j: %d\n",j);
+        kprintf("Value of value: %d\n",value);
+        kprintf("Value of sem value: %d\n",usr_sem_table[j].value);
+        spinlock_release(&sem_table_slock);
+        kprintf("Value of j: %d\n",j);
+        kprintf("Value of value: %d\n",value);
+        kprintf("Value of sem value: %d\n",usr_sem_table[j].value);
         return &usr_sem_table[j];
       }
     }
 
-    //spinlock_release(&sem_table_slock);
+    spinlock_release(&sem_table_slock);
     return NULL;
   }
   else {
     for (int k = 0; k < MAX_SEMAPHORES; k++) {
       if (stringcmp(usr_sem_table[k].name, name)) {
 
-        //spinlock_release(&sem_table_slock);
+        spinlock_release(&sem_table_slock);
         return &usr_sem_table[k]; // return handle already in use
       }
     }
-    //spinlock_release(&sem_table_slock);
+    spinlock_release(&sem_table_slock);
     return NULL;
   }
 }
 
 
 int usr_sem_destroy(usr_sem_t* sem) {
+  kprintf("1\n");
+  int test = sem->value;
+  kprintf("Test value is: %d\n",test);
   if (sem->value <= 0) {
+    kprintf("2\n");
     return -1; // fail, the semaphore is blocking
   }
   else {
+    kprintf("4\n");
     sem->value = 0;  // this might be hacky
+    kprintf("5\n");
     sem->name = NULL;
     return 0;
   }
 }
 
 int usr_sem_procure(usr_sem_t* sem) {
-  //interrupt_status_t intr_status;
+  interrupt_status_t intr_status;
 
-  //intr_status = _interrupt_disable();
-  //spinlock_acquire(&(sem->sem_slock));
+  intr_status = _interrupt_disable();
+  spinlock_acquire(&(sem->sem_slock));
 
   sem->value--;
   while (sem->value < 0) {
-    //sleepq_add(&(sem->value));
-    //spinlock_release(&(sem->sem_slock));
-    //thread_switch();
-    //spinlock_acquire(&(sem->sem_slock));
+    sleepq_add(&(sem->value));
+    spinlock_release(&(sem->sem_slock));
+    thread_switch();
+    spinlock_acquire(&(sem->sem_slock));
   }
 
-  //spinlock_release(&(sem->sem_slock));
-  //_interrupt_set_state(intr_status);
+  spinlock_release(&(sem->sem_slock));
+  _interrupt_set_state(intr_status);
   return 0;
 }
 
 int usr_sem_vacate(usr_sem_t* sem) {
-  //interrupt_status_t intr_status;
+  interrupt_status_t intr_status;
 
-  //intr_status = _interrupt_disable();
-  //spinlock_acquire(&(sem->sem_slock));
+  intr_status = _interrupt_disable();
+  spinlock_acquire(&(sem->sem_slock));
 
   sem->value++;
   if (sem->value <= 0) {
-    //sleepq_wake(&(sem->value));
+    sleepq_wake(&(sem->value));
   }
 
-  //spinlock_release(&(sem->sem_slock));
-  //_interrupt_set_state(intr_status);
+  spinlock_release(&(sem->sem_slock));
+  _interrupt_set_state(intr_status);
   return 0;
 }
--- g3/kudos/userland/sem.c
+++ g3/kudos/userland/sem.c
@@ -3,8 +3,10 @@
  */
 
 #include "lib.h"
+#include "lib/types.h"
 
-int main(void) {
-  syscall_usr_sem_open("test_sem", 1);
+int main (void) {
+  usr_sem_t* test_sem = syscall_usr_sem_open("test_sem", 5);
+  syscall_usr_sem_destroy(&test_sem);
   return 0;
 }
