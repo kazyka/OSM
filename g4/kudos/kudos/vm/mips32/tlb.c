bd4183f11d2a7eebe55dcf53ae9e04b929fa69a9
Jeppeks1 <Jeppeks1@hotmail.com>
2016-03-10 18:52:40 +0100

    tlb.c final version

--- g4/kudos/kudos/vm/mips32/tlb.c
+++ g4/kudos/kudos/vm/mips32/tlb.c
@@ -2,25 +2,90 @@
  * TLB handling
  */
 
+#include "lib/libc.h"
+#include "kernel/thread.h"
 #include "kernel/panic.h"
 #include "kernel/assert.h"
+#include "proc/process.h"
 #include <tlb.h>
 #include <pagetable.h>
 
-void tlb_modified_exception(void)
+#define EXCEPTION_USERLAND 0
+#define EXCEPTION_KERNEL 1
+
+int search_pagetable(pagetable_t* pagetable, int* entry_index)
 {
-  KERNEL_PANIC("Unhandled TLB modified exception");
+  // Search pagetable for matching entry belonging
+  // to the thread, that caused the exception.
+
+  tlb_exception_state_t tes;
+  _tlb_get_exception_state(&tes);
+
+  for (int i = 0; i < PAGETABLE_ENTRIES; i++){
+    if (tes.badvpn2 == pagetable->entries[i].VPN2){
+      *entry_index = i;
+      return 0; // Found a match
+    }
+  }
+
+  return -1; // Did not find a match
 }
 
-void tlb_load_exception(void)
+void tlb_modified_exception(int exception_origin)
 {
-  KERNEL_PANIC("Unhandled TLB load exception");
+  int dummy_val = 3;
+
+  switch (exception_origin) {
+  case EXCEPTION_USERLAND:
+    process_exit(dummy_val);
+    break;
+  case EXCEPTION_KERNEL:
+    KERNEL_PANIC("TLB Modify: Kernel panic");
+    break;
+  default:
+    KERNEL_PANIC("TLB Modify: Unhandled function argument");
+    break;
+  }
 }
 
-void tlb_store_exception(void)
+void tlb_load_exception(int exception_origin)
 {
-  KERNEL_PANIC("Unhandled TLB store exception");
+  kprintf("TLB Load started\n");
+  int search_pagetable_result, entry_index;
+  pagetable_t* pagetable = thread_get_current_thread_entry()->pagetable;
+
+  search_pagetable_result = search_pagetable(pagetable,&entry_index);
+  int dummy_val = 3;
+
+  kprintf("Search pagetable yielded result: %d\n", search_pagetable_result);
+
+  if (search_pagetable_result == 0) {
+    tlb_entry_t entry = pagetable->entries[entry_index];
+    _tlb_write_random(&entry);
+  }
+  else if (search_pagetable_result == -1) {
+      switch (exception_origin) {
+      case EXCEPTION_USERLAND:
+        process_exit(dummy_val);
+        break;
+      case EXCEPTION_KERNEL:
+        KERNEL_PANIC("TLB Load: Kernel panic");
+        break;
+      default:
+        KERNEL_PANIC("TLB Load: Unhandled function argument");
+        break;
+      }
+  }
+  else
+    KERNEL_PANIC("TLB Load: Unknown return value from search_pagetable");
 }
+
+void tlb_store_exception(int exception_origin)
+{
+  // The load and store exceptions are handled the same
+  tlb_load_exception(exception_origin);
+}
+
 
 /**
  * Fill TLB with given pagetable. This function is used to set memory
