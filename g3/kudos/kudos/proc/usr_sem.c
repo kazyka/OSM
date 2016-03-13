50ced30e396f15c47e72800dbda45ad8b5c2e4c3
Jeppeks1 <Jeppeks1@hotmail.com>
2016-03-13 20:51:55 +0100

    Update usr_sem.c

--- g3/kudos/kudos/proc/usr_sem.c
+++ g3/kudos/kudos/proc/usr_sem.c
@@ -11,14 +11,17 @@ static usr_sem_t usr_sem_table[MAX_SEMAPHORES];
 
 usr_sem_t* usr_sem_open(const char* name, int value) {
 
+  interrupt_status_t intr_status;
+  intr_status = _interrupt_disable();
+
   spinlock_acquire(&sem_table_slock);
 
   if (value >= 0) {
     for (int i = 0; i < MAX_SEMAPHORES; i++) {
       if (usr_sem_table[i].name != NULL) {
         if (!stringcmp(usr_sem_table[i].name, name)) {
-          kprintf(usr_sem_table[i].name);
           spinlock_release(&sem_table_slock);
+          _interrupt_set_state(intr_status);
           return NULL;
         }
       }
@@ -26,20 +29,15 @@ usr_sem_t* usr_sem_open(const char* name, int value) {
 
     for (int j = 0; j < MAX_SEMAPHORES; j++) {
       if (usr_sem_table[j].name == NULL) {
-        usr_sem_table[j].name = name;
+        stringcopy(usr_sem_table[j].name, name, 8);
         usr_sem_table[j].value = value;
-        kprintf("Value of j: %d\n",j);
-        kprintf("Value of value: %d\n",value);
-        kprintf("Value of sem value: %d\n",usr_sem_table[j].value);
         spinlock_release(&sem_table_slock);
-        kprintf("Value of j: %d\n",j);
-        kprintf("Value of value: %d\n",value);
-        kprintf("Value of sem value: %d\n",usr_sem_table[j].value);
         return &usr_sem_table[j];
       }
     }
 
     spinlock_release(&sem_table_slock);
+    _interrupt_set_state(intr_status);
     return NULL;
   }
   else {
@@ -51,23 +49,19 @@ usr_sem_t* usr_sem_open(const char* name, int value) {
       }
     }
     spinlock_release(&sem_table_slock);
+    _interrupt_set_state(intr_status);
     return NULL;
   }
 }
 
 
 int usr_sem_destroy(usr_sem_t* sem) {
-  kprintf("1\n");
-  int test = sem->value;
-  kprintf("Test value is: %d\n",test);
+
   if (sem->value <= 0) {
-    kprintf("2\n");
     return -1; // fail, the semaphore is blocking
   }
   else {
-    kprintf("4\n");
     sem->value = 0;  // this might be hacky
-    kprintf("5\n");
     sem->name = NULL;
     return 0;
   }
@@ -75,8 +69,8 @@ int usr_sem_destroy(usr_sem_t* sem) {
 
 int usr_sem_procure(usr_sem_t* sem) {
   interrupt_status_t intr_status;
-
   intr_status = _interrupt_disable();
+
   spinlock_acquire(&(sem->sem_slock));
 
   sem->value--;
@@ -94,8 +88,8 @@ int usr_sem_procure(usr_sem_t* sem) {
 
 int usr_sem_vacate(usr_sem_t* sem) {
   interrupt_status_t intr_status;
-
   intr_status = _interrupt_disable();
+
   spinlock_acquire(&(sem->sem_slock));
 
   sem->value++;
