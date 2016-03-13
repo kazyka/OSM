10211987169ad4ea6170b1265265ff06745cd1d7
Jeppeks1 <Jeppeks1@hotmail.com>
2016-03-13 20:52:24 +0100

    Update usr_sem.h

--- g3/kudos/kudos/proc/usr_sem.h
+++ g3/kudos/kudos/proc/usr_sem.h
@@ -1,7 +1,9 @@
 #define MAX_SEMAPHORES 128
 
+#include "kernel/spinlock.h"
+
 typedef struct {
-  const char* name;
+  char* name;
   int value;
   spinlock_t sem_slock;
 } usr_sem_t;
@@ -11,5 +13,3 @@ usr_sem_t* usr_sem_open(const char* name, int value);
 int usr_sem_destroy(usr_sem_t* sem);
 int usr_sem_procure(usr_sem_t* sem);
 int usr_sem_vacate(usr_sem_t* sem);
-
-
